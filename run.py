bbe5fab62cf483bc626470319da3903b514a65ab
berat.zorlu <berat.zorlu@hotmail.com>
2023-01-14 23:31:33 +0000

    Add core program variables

---
+++ run.py
@@ -0,0 +1,13 @@
+QUESTIONS = ("Question1",  
+             "Question2", 
+             "Question3", 
+             "Question4", 
+             "Question5")
+             
+CHOICES = ((), (), (), (), ())
+
+quiz_answers = ()
+quiz_guesses = []
+user_score = 0
+question_number = 0
+
