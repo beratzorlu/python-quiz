32f8b7bb0bfe36845fe4393e866989e8d45f47b7
berat.zorlu <berat.zorlu@hotmail.com>
2023-01-14 23:37:04 +0000

    Add placeholder quiz content for testing

--- run.py
+++ run.py
@@ -3,10 +3,14 @@ QUESTIONS = ("Question1",
              "Question3", 
              "Question4", 
              "Question5")
-             
-CHOICES = ((), (), (), (), ())
 
-quiz_answers = ()
+CHOICES = (("I. Choice1", "II. Choice2", "III. Choice3", "IV. Choice4"),
+           ("I. Choice1", "II. Choice2", "III. Choice3", "IV. Choice4"),
+           ("I. Choice1", "II. Choice2", "III. Choice3", "IV. Choice4"),
+           ("I. Choice1", "II. Choice2", "III. Choice3", "IV. Choice4"),
+           ("I. Choice1", "II. Choice2", "III. Choice3", "IV. Choice4"))
+
+quiz_answers = ("I", "I", "I", "I", "I")
 quiz_guesses = []
 user_score = 0
 question_number = 0
