311f9c4d41b40b94f06f1eb5132823660b50e4fc
berat.zorlu <berat.zorlu@hotmail.com>
2023-01-15 19:33:58 +0000

    Change question and answer tuples to dictionary

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
 #Quiz
 
-QUESTIONS = ("Question1",  
-             "Question2", 
-             "Question3", 
-             "Question4", 
-             "Question5")
-
-CHOICES = (("I. Choice1", "II. Choice2", "III. Choice3", "IV. Choice4"),
-           ("I. Choice11", "II. Choice22", "III. Choice33", "IV. Choice44"),
-           ("I. Choice111", "II. Choice222", "III. Choice333", "IV. Choice444"),
-           ("I. Choice1111", "II. Choice2222", "III. Choice3333", "IV. Choice4444"),
-           ("I. Choice11111", "II. Choice22222", "III. Choice33333", "IV. Choice44444"))
+QUIZ_QUESTIONS = [
+    {"question": "QUESTION 1",
+    "choices": "1. Choice1", "2. Choice2", "3. Choice3", "4. Choice4",
+    "answer": 1},
+    {"question": "QUESTION 2",
+    "choices": "1. Choice1", "2. Choice2", "3. Choice3", "4. Choice4",
+    "answer": 1},
+    {"question": "QUESTION 3",
+    "choices": "1. Choice1", "2. Choice2", "3. Choice3", "4. Choice4",
+    "answer": 1},
+    {"question": "QUESTION 4",
+    "choices": "1. Choice1", "2. Choice2", "3. Choice3", "4. Choice4",
+    "answer": 1},
+    {"question": "QUESTION 5",
+    "choices": "1. Choice1", "2. Choice2", "3. Choice3", "4. Choice4",
+    "answer": 1},
+]
 
 quiz_answers = ("I", "I", "I", "I", "I")
 user_guesses = []
