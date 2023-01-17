32bab6d08be12bf4623fefc711faa0659333ea9a
berat.zorlu <berat.zorlu@hotmail.com>
2023-01-17 13:28:41 +0000

    Add replay, score accuracy and docstrings.

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 """
-Import questions and choices to be utilized by the program.
-Execute the quiz functionality
-Push user data to databases
+Import libraries essential to the function of this application.
 """
 from pprint import pprint
 from questions import QUIZ_QUESTIONS
@@ -9,6 +7,17 @@ from questions import QUIZ_CHOICES
 
 
 def run_new_quiz():
+    """
+    Display input information to the player.
+    Iterate through the questions list.
+    Iterate through the choices list.
+    Ask for question answer attempt from the user.
+    Append the chosen answer of the user to answer_attempts list.
+    Validate user input.
+    Increment question number by 1.
+    Display user score after last question.
+
+    """
     answer_attempts = []
     user_guesses_correct = 0
     current_question_num = 1
@@ -29,22 +38,54 @@ def run_new_quiz():
 
 
 def validate_user_input(correct_answer, answer_attempt):
+    """ 
+    Compare the correct answer with user's answer attempt.
+    Display feedback depending on the correctness of user's answer.
+    """
     if correct_answer == answer_attempt:
         print("Your answer is correct!\n")
         return 1
-    else:
-        print("Incorrect answer.\n")
-        return 0
+    print("Incorrect answer.\n")
+    return 0
 
 
 def display_user_score(user_guesses_correct, answer_attempts):
+    """
+    Print informative text about quiz completion and score calculation.
+    Iterate through the questions list to extract and display correct answers.
+    Iterate through answer_attempts to display user's answer attempt history.
+    Calculate a percentage value to display user's overall accuracy.
+    """
+    print(' ')
+    print('--\n')
+    print('You have completed the quiz, calculating results...\n')
+    print('--')
     print("Results")
+    print('--')
     print("Answers: ", end=" ")
     for ind in QUIZ_QUESTIONS:
         print(QUIZ_QUESTIONS.get(ind), end=" ")
+    print(' ')
     print("Your choices: ", end=" ")
     for ind in answer_attempts:
         print(ind, end=" ")
+    print(' ')
+    final_score = int((user_guesses_correct/len(QUIZ_QUESTIONS))*100)
+    print(f'You have performed with an accuracy of "{str(final_score)}%"')
+
+
+def replay_quiz():
+    """
+    Ask user if they wish to play the quiz again.
+    Depending on their choice either;
+        -If yes, start the quiz again.
+        -If no, terminate the application.
+    """
+    replay = input('Would you like to try again? (Y/N)\n')
+    replay = replay.upper()
+    if replay == "Y":
+        return True
+    return False
 
    
 def main():
@@ -52,10 +93,14 @@ def main():
     Execute all fundamental program functions.
     """
     run_new_quiz()
-    # validate_user_input(QUIZ_QUESTIONS.get(key), answer_attempt)
-    # display_user_score()
-    # replay_quiz()
+    replay_quiz()
 
+
+while replay_quiz():
+    run_new_quiz()
+    print('Ending quiz and saving user data...')
+    print('Terminating application...')
+    print('Thank you for playing.')
 
 main()
 
