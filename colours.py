0446e1a62192a1fd8cff8ad22685364be389b0bf
berat.zorlu <berat.zorlu@hotmail.com>
2023-01-20 15:50:15 +0000

    Add docstring to QuizColours() class.

--- colours.py
+++ colours.py
@@ -1,17 +1,16 @@
 """
 Colour library for text stylisation for the application.
 """
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore
 
 init(autoreset=True)
 
 
 class QuizColours():
+    """
+    Class contains shortcuts to various colours used in the application.
+    """
     G = Fore.GREEN
     R = Fore.RED
     B = Fore.BLUE
     Y = Fore.YELLOW
-    BG = Back.GREEN
-    BR = Back.RED
-    BB = Back.BLUE
-    BY = Back.YELLOW
